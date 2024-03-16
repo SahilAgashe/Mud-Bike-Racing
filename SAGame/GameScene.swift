ab40cacca8c8e49b9fff90db62a71e04f75a2420
Sahil Agashe <sahil@scrambleapps.gmail.com>
2024-03-16 15:54:05 +0530

    affectedByGravity bug fixing. Added collision detection functionality.

--- SAGame/GameScene.swift
+++ SAGame/GameScene.swift
@@ -32,6 +32,12 @@ class GameScene: SKScene {
         addChild(player)
         
         gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+        
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.affectedByGravity = false
+        player.physicsBody?.categoryBitMask = 1
+        
+        physicsWorld.contactDelegate = self
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -65,18 +71,44 @@ class GameScene: SKScene {
     // MARK: - Selectors
     
     @objc private func createEnemy() {
-        
         let randomDistribution = GKRandomDistribution(lowestValue: -350, highestValue: 350)
         let sprite = SKSpriteNode(imageNamed: "car")
+        // asteroid can also be enemy
         
-        sprite.position = CGPoint(x: 200, y: randomDistribution.nextInt())
+        sprite.position = CGPoint(x: 1000, y: randomDistribution.nextInt())
         sprite.name = "enemy"
         sprite.zPosition = 1
         addChild(sprite)
         
         sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
         sprite.physicsBody?.velocity = CGVector(dx: -1000, dy: 0)
-        sprite.physicsBody?.linearDamping = 0.0
-        sprite.physicsBody?.angularDamping = 0.0
+        //sprite.physicsBody?.linearDamping = 0.0
+        //sprite.physicsBody?.angularDamping = 0.0
+        sprite.physicsBody?.affectedByGravity = false
+        
+        sprite.physicsBody?.contactTestBitMask = 1
+        sprite.physicsBody?.categoryBitMask = 0
     }
+    
+    // MARK: - Helpers
+    
+    private func playerHit(_ node: SKNode) {
+        player.removeFromParent()
+    }
+}
+
+extension GameScene: SKPhysicsContactDelegate {
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard let nodeA = contact.bodyA.node else { return }
+        guard let nodeB = contact.bodyB.node else { return }
+        
+        if nodeA == player {
+            playerHit(nodeB)
+        } else {
+            playerHit(nodeA)
+        }
+    }
+    
+    
 }
